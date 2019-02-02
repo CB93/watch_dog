2729c1c09c9886ebe34c05a19f86b3f1a302079b
tkyc@arch <terrykyc.7000@gmail.com>
2019-02-02 13:23:54 -0800

    Added google map obj

---
+++ app/src/main/java/com/example/watch_dog/MapsActivity.java
@@ -0,0 +1,37 @@
+package com.example.watch_dog;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Focus camera on Maple Ridge and add marker
+        LatLng mapleRidge = new LatLng(49.2193, -122.60193);
+        mMap.addMarker(new MarkerOptions().position(mapleRidge).title("Marker in Maple Ridge"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mapleRidge));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+    }
+}
